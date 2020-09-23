d0aac0731666bf9fe530d316db5c292956cf88bb
Julia Bauer <julia.jjbauer@gmail.com>
2020-09-23 20:19:34 -0300

    aula 6 - tupla e vetor

---
+++ Aulas/Aula6/tupla.py
@@ -0,0 +1,40 @@
+carros = ("Uno", "Doblo", "Toro")
+print(carros[1])
+print("-------------------------------")
+
+for car in carros:
+    print(car)
+
+print("-------------------------------")
+
+print(carros[1:2])
+
+print("-------------------------------")
+
+print(carros[1:3])
+
+print("-------ORDENADO-------")
+
+print( sorted(carros))
+
+print("---------FOR-COM-RANGE---------")
+
+for car in range( len(carros) - 2 ):
+
+    print("posição" + str(car) + ":" + carros[car])
+
+print("-------USANDO-FUNÇÃO-------")
+
+def calcular(x, y):
+    return(x+y), (x-y), (x*y), (x/y)
+
+print (calcular(5, 2))
+
+print("-------------------------------")
+
+vet = [1,5,20]
+vet = [100,50,20,60]
+vet[1] = 10
+vet.sort()
+print(vet)
+
